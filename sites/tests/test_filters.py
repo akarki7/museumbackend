329e1be1f0369f63ab11a5a6d88482e7ad7c7284
aabishkar.karki <aabishkar.karki@finn.auto>
2021-11-01 17:35:46 +0100

    Finished tests for filters

--- sites/tests/test_filters.py
+++ sites/tests/test_filters.py
@@ -2,7 +2,7 @@ from django.test.testcases import TestCase
 import pytest
 from datetime import date,datetime,timedelta
 from rest_framework.test import APIRequestFactory, force_authenticate, APIClient
-from sites.models import Site, Videos, Photos
+from sites.models import Site, Videos, Photos, TimePeriod
 from sites.views import SitesViewSet
 from users.models import User
 
@@ -17,11 +17,11 @@ class TestSiteFilter(TestCase):
         self.preexisting_site_1 = Site.objects.create(
             serial_number=1,
             title="BurgerPark",
-            file_name="Keine Ahnung",
+            file_name="VGS",
             file_type=".docx",
             original_scan="false",
-            time_period="G1",
-            origin_date="2021-01-11",
+            time_period=TimePeriod.FL,
+            origin_date=date.today(),
             author="Dr Frank",
             size=10,
             description="Dummy_file",
@@ -37,11 +37,31 @@ class TestSiteFilter(TestCase):
         self.preexisting_site_2 = Site.objects.create(
             serial_number=2,
             title="Vegesack",
-            file_name="VGS",
+            file_name="LAX",
             file_type=".docx",
             original_scan="false",
-            time_period="G1",
-            origin_date="2021-01-11",
+            time_period=TimePeriod.FL,
+            origin_date=date.today(),
+            author="Dr Khan",
+            size=10,
+            description="Dummy_test",
+            source="The Eternals",
+            signature="IDK",
+            copyright_status="present",
+            current_location="Bremen",
+            found_by="Herr Mueller",
+            found_date="2002-10-06",
+            production_date="2005-05-05",
+            additional_info="Nothing more to add"
+        )
+        self.preexisting_site_3 = Site.objects.create(
+            serial_number=3,
+            title="Vegesack",
+            file_name="LAX",
+            file_type=".docx",
+            original_scan="false",
+            time_period=TimePeriod.G1,
+            origin_date=date.today() + timedelta(days=10),
             author="Dr Khan",
             size=10,
             description="Dummy_test",
@@ -67,4 +87,84 @@ class TestSiteFilter(TestCase):
         response= self.client.get(f"/api/sites/?title={fake_title}")
         assert response.status_code == 200
         assert len(response.data)==0
-        
+
+    def test_filter_title_yields_one_results(self):
+        fake_title="BurgerPark"
+        response= self.client.get(f"/api/sites/?title={fake_title}")
+        assert response.status_code == 200
+        assert len(response.data)==1
+            
+    def test_filter_title_yields_two_results(self):
+        fake_title="Vegesack"
+        response= self.client.get(f"/api/sites/?title={fake_title}")
+        assert response.status_code == 200
+        assert len(response.data)==2
+
+    def test_filter_file_name_yields_no_results(self):
+        fake_name="Holalala"
+        response= self.client.get(f"/api/sites/?file_name={fake_name}")
+        assert response.status_code == 200
+        assert len(response.data)==0
+
+    def test_filter_file_name_yields_one_results(self):
+        fake_name="VGS"
+        response= self.client.get(f"/api/sites/?file_name={fake_name}")
+        assert response.status_code == 200
+        assert len(response.data)==1
+            
+    def test_filter_file_name_yields_two_results(self):
+        fake_name="LAX"
+        response= self.client.get(f"/api/sites/?file_name={fake_name}")
+        assert response.status_code == 200
+        assert len(response.data)==2
+
+    def test_filter_origin_date_yields_no_results(self):
+        fake_date=date.today() + timedelta(days=2)
+        response= self.client.get(f"/api/sites/?origin_date={fake_date}")
+        assert response.status_code == 200
+        assert len(response.data)==0
+
+    def test_filter_origin_date_yields_one_results(self):
+        fake_date=date.today() + timedelta(days=10)
+        response= self.client.get(f"/api/sites/?origin_date={fake_date}")
+        assert response.status_code == 200
+        assert len(response.data)==1
+            
+    def test_filter_origin_date_yields_two_results(self):
+        fake_date=date.today()
+        response= self.client.get(f"/api/sites/?origin_date={fake_date}")
+        assert response.status_code == 200
+        assert len(response.data)==2
+
+    def test_filter_serial_number_yields_no_results(self):
+        fake_num=65
+        response= self.client.get(f"/api/sites/?serial_number={fake_num}")
+        assert response.status_code == 200
+        assert len(response.data)==0
+
+    def test_filter_serial_number_yields_one_results(self):
+        fake_num=2
+        response= self.client.get(f"/api/sites/?serial_number={fake_num}")
+        assert response.status_code == 200
+        assert len(response.data)==1
+
+    def test_filter_time_period_yields_no_results(self):
+        fake_period=TimePeriod.DP
+        response= self.client.get(f"/api/sites/?time_period={fake_period}")
+        assert response.status_code == 200
+        assert len(response.data)==0
+
+    def test_filter_time_period_yields_one_results(self):
+        fake_period=TimePeriod.G1
+        response= self.client.get(f"/api/sites/?time_period={fake_period}")
+        assert response.status_code == 200
+        assert len(response.data)==1
+            
+    def test_filter_time_period_yields_two_results(self):
+        fake_period=TimePeriod.FL
+        response= self.client.get(f"/api/sites/?time_period={fake_period}")
+        assert response.status_code == 200
+        assert len(response.data)==2
+
+
+    
