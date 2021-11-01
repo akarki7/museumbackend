ee1b3661d3e390008bd28f1846ffc95be462f2c8
aabishkar.karki <aabishkar.karki@finn.auto>
2021-11-01 15:47:19 +0100

    Finished adding filters

--- sites/filters.py
+++ sites/filters.py
@@ -1,10 +1,19 @@
 from djano_filters import rest_framework as filters
-from sites.models import Site, Videos, Photos
+from sites.models import Site, TimePeriod
 
 class SiteFilter(filters.FilterSet):
     file_name=filters.CharFilter(field_name="file_name")
     origin_date=filters.DateFilter(field_name="origin_date")
-    serial_numer=filters.CharFilter(field_name="serial_numer")
+    serial_number=filters.CharFilter(field_name="serial_number")
     title =filters.CharFilter(field_name="title")
-    
-#file_name, origin_date, current_location, serial_number, time_period, title
+    time_period = filters.ChoiceFilter(field_name="time_period", choices=TimePeriod.choices)
+
+    class Meta:
+        model= Site
+        fields=(
+            "file_name",
+            "origin_date",
+            "serial_number",
+            "title",
+            "time_period",
+        )
